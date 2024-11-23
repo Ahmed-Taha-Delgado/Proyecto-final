88588508abe4619224ae7e0dceed3bd68850502c
Ahmed-Taha-Delgado <ahmedtmtd@gmail.com>
2024-11-23 20:23:45 +0000

    Co-authored-by: Gabrielvlzq <Gabrielvlzq@users.noreply.github.com> Co-authored-by: Primo-Flapi <Primo-Flapi@users.noreply.github.com> Co-authored-by: Ivandx12 <Ivandx12@users.noreply.github.com> Co-authored-by: chevimeneses <chevimeneses@users.noreply.github.com> Co-authored-by: Ddy81 <Ddy81@users.noreply.github.com> Co-authored-by: codeAzael <codeAzael@users.noreply.github.com>

--- proyecto.c
+++ proyecto.c
@@ -1,10 +1,119 @@
 #include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
 
+int capacidadat_max=60;
+int capacidadac_max=40;
+int capacidadvag_max=15;
+int autobusest_max=5;
+int autobusesc_max=2;
+int vagonetas_max=4;
+char nombre[];
+char correo[100];
+char telefono[1;
 
-int main()
-{
-  puts("Hola mundo");
-  puts("No");
-  return 0;
+  printf("...[Registro del usuario]...\n")
+  printf("Ingrese su nombre completo")
+  scanf("%")
+  
+int autobust (){
+  int matriz [][]=
 
+  
 }
+
+
+
+
+
+
+int destino CDMX(){
+  int matriz [10][6]={
+    {'0','0','0','  ','0','0','0'}
+    {'0','0','0','  ','0','0','0'}
+    {'0','0','0','  ','0','0','0'}
+    {'0','0','0','  ','0','0','0'}
+    {0,0,0,  ,0,0,0}
+    {0,0,0,  ,0,0,0}
+    {0,0,0,  ,0,0,0}
+    {0,0,0,  ,0,0,0}
+    {0,0,0,  ,0,0,0}
+    {0,0,0,  ,0,0,0}
+  }
+}
+
+
+int destino Puebla(){
+  int matriz [10][4]={
+    {'0',0,  ,0,0}
+    {'0',0,  ,0,0}
+    {'0,0,  ,0,0}
+    {'0,0,  ,0,0}
+    {'0,0,  ,0,0}
+    {'0,0,  ,0,0}
+    {'0,0,  ,0,0}
+    {'0,0,  ,0,0}
+    {'0,0,  ,0,0}
+    {'0,0,  ,0,0}
+
+  }
+}
+
+int destino Pueblos MagicosV
+
+
+
+
+int main(){
+ 
+  printf("Ingresa tu nombre: ");
+  scanf("%s", &nombre);  
+ 
+  puts("Bienvenido a la compañia TravelBus");
+  puts("Explora nuestras opciones de de viajes y reserva en minutos\n");
+  puts("¿Desea hacer una reservacion?");
+  puts("Presiona Enter para continuar");
+  getchar();
+
+  puts("¿A que destino deseas ir?\nEstos son nuestros destinos disponibles:\n");
+  puts("Viajes turisticos:");
+  puts("1.-Circuito Historico");
+  puts("2.-Tour Gastronómico");
+  puts("3.-Parque "La Mexicana"");
+  puts("4.-Universum\n");
+  puts("Viajes nacionales:");
+  puts("1.-CDMX-Puebla");
+  puts("2.-CDMX-Queretaro");
+  puts("3.-CDMX-Hidalgo\n");
+  puts("Pueblos mágicos:");
+  puts("1.-CDMX-Tepoztzotlán");
+  puts("2.-CDMX-Valle de Bravo");
+
+
+
+
+  switch (Destino)
+  {
+    case CDMX:
+        int destino CDMX
+    break;
+
+    case Nacionales:
+          
+    break;
+        
+    case Pueblos Magicos:
+          
+    break;
+   
+     default:
+      printf("EL autobus que busca no es del tipo que la empresa tiene");
+  }
+
+  return 0;
+}
+
+
+
