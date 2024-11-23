2731db00499ef61696d600127d3706267e0f9807
Ahmed-Taha-Delgado <ahmedtmtd@gmail.com>
2024-11-23 21:10:25 +0000

    Co-authored-by: Gabrielvlzq <Gabrielvlzq@users.noreply.github.com> Co-authored-by: Primo-Flapi <Primo-Flapi@users.noreply.github.com> Co-authored-by: Ivandx12 <Ivandx12@users.noreply.github.com> Co-authored-by: chevimeneses <chevimeneses@users.noreply.github.com> Co-authored-by: Ddy81 <Ddy81@users.noreply.github.com> Co-authored-by: codeAzael <codeAzael@users.noreply.github.com>

--- proyecto.c
+++ proyecto.c
@@ -10,101 +10,244 @@ int capacidadvag_max=15;
 int autobusest_max=5;
 int autobusesc_max=2;
 int vagonetas_max=4;
-char nombre[];
-char correo[100];
-char telefono[1;
 
-  printf("...[Registro del usuario]...\n")
-  printf("Ingrese su nombre completo")
-  scanf("%")
+int registro(){
+
+  FILE *file = fopen("Registro de Usuarios.txt", "w");
+    if (file == NULL){
+      printf("Error al abrir el archivo. \n");
+    }
+    
+      char nombre[100];
+      char correo[100];
+      char telefono[15];
   
-int autobust (){
-  int matriz [][]=
-
+  printf("...[Registro del usuario]...\n");
+  printf("Ingrese su nombre completo: ");
+  fgets(nombre, sizeof(nombre), stdin);
+  printf("Ingrese su correo electronico: ");
+  fgets(correo, sizeof(correo), stdin);
+  printf("Ingrese su numero de telefono: ");
+  fgets(telefono, sizeof(telefono), stdin);
   
+  fprintf(file, "Nombre: %s", nombre);
+  fprintf(file, "Correo: %s", correo);
+  fprintf(file, "Telefono: %s", telefono);
+  fclose(file);
+  printf("Su registro se ha concretado\n");
+  
+  
+  return 0;
 }
 
 
 
 
-
-
-int destino CDMX(){
-  int matriz [10][6]={
-    {'0','0','0','  ','0','0','0'}
-    {'0','0','0','  ','0','0','0'}
-    {'0','0','0','  ','0','0','0'}
-    {'0','0','0','  ','0','0','0'}
-    {0,0,0,  ,0,0,0}
-    {0,0,0,  ,0,0,0}
-    {0,0,0,  ,0,0,0}
-    {0,0,0,  ,0,0,0}
-    {0,0,0,  ,0,0,0}
-    {0,0,0,  ,0,0,0}
-  }
+int destinoCircuitoHistorico(){
+ char matriz[10][7]={
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}
+      };
+ 
+}
+int destinoTourGastronmico (){
+ char matriz[10][7]={
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}
+      };
+ 
+}
+int destinoParqueLaMexicana(){
+ char matriz[10][7]={
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}
+      };
+ 
+}
+int destinoUniversum(){
+ char matriz[10][7]={
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}
+      };
+ 
 }
 
-
-int destino Puebla(){
-  int matriz [10][4]={
-    {'0',0,  ,0,0}
-    {'0',0,  ,0,0}
-    {'0,0,  ,0,0}
-    {'0,0,  ,0,0}
-    {'0,0,  ,0,0}
-    {'0,0,  ,0,0}
-    {'0,0,  ,0,0}
-    {'0,0,  ,0,0}
-    {'0,0,  ,0,0}
-    {'0,0,  ,0,0}
-
-  }
+int destinoPuebla(){
+  char matriz [10][5]={
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'}
+    
+  };
 }
 
-int destino Pueblos MagicosV
+int destinoQueretaro(){
+  char matriz [10][5]={
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'}
+    
+  };
+}
 
+int destinoHidalgo(){
+  char matriz [10][5]={
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'},
+    {'L', 'L',' ','L', 'L'}
+    
+  };
+}
 
+int destinoTepotzotlan(){
+   char matriz[5][3] = {
+    {'L','L','L'},
+    {'L','L','L'},
+    {'L','L','L'},
+    {'L','L','L'},
+    {'L','L','L'}
+
+    };
+   
+  return 0;
+}
+
+int destinoValleDelBravo(){
+   char matriz[5][3] = {
+    {'L','L','L'},
+    {'L','L','L'},
+    {'L','L','L'},
+    {'L','L','L'},
+    {'L','L','L'}
+
+    };
+
+    
+   
+  return 0;
+}
 
 
 int main(){
- 
-  printf("Ingresa tu nombre: ");
-  scanf("%s", &nombre);  
- 
+  char Tipo;
+
   puts("Bienvenido a la compañia TravelBus");
-  puts("Explora nuestras opciones de de viajes y reserva en minutos\n");
+  puts("Explora nuestras opciones de viajes y reserva en minutos\n");
   puts("¿Desea hacer una reservacion?");
   puts("Presiona Enter para continuar");
   getchar();
 
-  puts("¿A que destino deseas ir?\nEstos son nuestros destinos disponibles:\n");
-  puts("Viajes turisticos:");
-  puts("1.-Circuito Historico");
-  puts("2.-Tour Gastronómico");
-  puts("3.-Parque "La Mexicana"");
-  puts("4.-Universum\n");
-  puts("Viajes nacionales:");
-  puts("1.-CDMX-Puebla");
-  puts("2.-CDMX-Queretaro");
-  puts("3.-CDMX-Hidalgo\n");
-  puts("Pueblos mágicos:");
-  puts("1.-CDMX-Tepoztzotlán");
-  puts("2.-CDMX-Valle de Bravo");
+  puts("Ingresa tu nombre completo");
+  scanf("%s",nombre);
+  
+  puts("Ingresa tu correo electronico");
+  scanf("%s",correo);
+  
+  puts("Estos son nuestros viajes disponibles:\n");
+  puts("a) Viajes turisticos:");
+  puts("b) Viajes nacionales:");
+  puts("c) Pueblos mágicos:");
+  puts("Selecciona una letra");
+  scanf("%c",&Tipo);
+  
 
 
+  switch (Tipo){
+    case a:
+      puts("¿A que destino deseas ir?");
+      puts("1.-Circuito Historico");
+      puts("2.-Tour Gastronómico");
+      puts("3.-Parque "La Mexicana"");
+      puts("4.-Universum\n");
+      scanf("%i",&op);
+      break;
+
+    case b:
+      puts("¿A que destino deseas ir?");
+      puts("1.-CDMX-Puebla");
+      puts("2.-CDMX-Queretaro");
+      puts("3.-CDMX-Hidalgo\n");
+      break;
+      
+    case c:
+      puts("¿A que destino deseas ir?");
+      puts("1.-CDMX-Tepoztzotlán");
+      puts("2.-CDMX-Valle de Bravo");
+      break;
+
+    default:
+        puts("Ingresa alguna de esas opciones");
+      break;
+  }
 
 
   switch (Destino)
   {
     case CDMX:
-        int destino CDMX
+        int destinoCDMX;
+        if (){
+        
+        }
     break;
 
     case Nacionales:
           
     break;
         
-    case Pueblos Magicos:
+    case Pueblos Mágicos:
           
     break;
    
