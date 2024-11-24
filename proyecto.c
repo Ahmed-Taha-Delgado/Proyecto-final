e3550902f1cc77bef4d74ddca5e7aa8dfebdcecb
Ahmed-Taha-Delgado <ahmedtmtd@gmail.com>
2024-11-24 00:31:46 +0000

    Co-authored-by: Gabrielvlzq <Gabrielvlzq@users.noreply.github.com> Co-authored-by: Primo-Flapi <Primo-Flapi@users.noreply.github.com> Co-authored-by: codeAzael <codeAzael@users.noreply.github.com> Co-authored-by: Ddy81 <Ddy81@users.noreply.github.com> Co-authored-by: Ivandx12 <Ivandx12@users.noreply.github.com> Co-authored-by: chevimeneses <chevimeneses@users.noreply.github.com>

--- proyecto.c
+++ proyecto.c
@@ -11,175 +11,71 @@ int autobusest_max=5;
 int autobusesc_max=2;
 int vagonetas_max=4;
 
-int tepotzotlan(){
-  for(int i=0;i<5;i++){
-    for(int j=0;j<3;j++){
-        printf("%c",matriz[i][j]);
-    }
-    printf("\n");
-  }
-  
-  puts("Ingresa la fila que deseas apartar (1-5)");
-  scanf("%d",&F);
-  puts("Ingresa la columna que deseas apartar (1-3)");
-  scanf("%d",&C);
-  
-  F=F-1;
-  C=C-1;
-  if(matriz[F][C]=='O'){
-    puts("El asiento ya esta ocupado");
-    printf("¿Deseas continuar con la reserva? (Y/N)\n");
-    getchar();
-    scanf("%c",&s);
-    if (s==Y || s==y){
-      destinoTepotzotlan();
-    }else{
-      printf("Muchas gracias por considerar viajar con nosotros.\n Hasta luego.");
-      system("exit");
+int generar_ticket(char *nombre, char *correo, char *telefono, char *destino, int ticket_numero) {
+    char nombre_archivo[150];
+
+    sprintf(nombre_archivo, "Tickets/Ticket_%d.txt", ticket_numero);
+
+
+    FILE *ticket = fopen(nombre_archivo, "w");
+    if (ticket == NULL) {
+        printf("Error al generar el archivo del ticket.\n");
+        return 0;
     }
 
-  }else{
-    if(F<5 && F>=0 && C<3 && C>=0){
-        puts("Su asiento ha sido reservado\n")
-        matriz[F][C]= 'O';
 
-    }else{ 
-      puts("EL numero de filas o columnas esta mal ingresado");
-      printf("¿Deseas continuar con la reserva? (Y/N)\n");
-      getchar();
-      scanf("%c",&s);
-      if (s==Y || s==y){
-        destinoTepotzotlan();
-      }else{
-        printf("Muchas gracias por considerar viajar con nosotros.\n Hasta luego.");
-        system("exit");
-      }
-    }
-  }
+    fprintf(ticket, "=======================\n");
+    fprintf(ticket, "=   TICKET DE VIAJE   =   \n");
+    fprintf(ticket, "=======================\n");
+    fprintf(ticket, "Nombre: %s", nombre);
+    fprintf(ticket, "Correo: %s", correo);
+    fprintf(ticket, "Telefono: %s", telefono);
+    fprintf(ticket, "Destino: %s\n", destino);
+    fprintf(ticket, "=======================\n");
 
-  for(int i=0;i<5;i++){
-    for(int j=0;j<3;j++){
-        printf("%c",matriz[i][j]);
-    }
-    printf("\n");
-  }
-  
-  return 0;
+    fclose(ticket);
+    printf("Ticket generado: %s\n", nombre_archivo);
+    return 0;
 }
-
-int valledebravo(){
-  for(int i=0;i<5;i++){
-    for(int j=0;j<3;j++){
-        printf("%c",matriz[i][j]);
-    }
-    printf("\n");
-  }
-  
-  puts("Ingresa la fila que deseas apartar (1-5)");
-  scanf("%d",&F);
-  puts("Ingresa la columna que deseas apartar (1-3)");
-  scanf("%d",&C);
-  
-  F=F-1;
-  C=C-1;
-  if(matriz[F][C]=='O'){
-    puts("El asiento ya esta ocupado");
-    printf("¿Deseas continuar con la reserva? (Y/N)\n");
-    getchar();
-    scanf("%c",&s);
-    if (s==Y || s==y){
-      destinoValleDelBravo();
-    }else{
-      printf("Muchas gracias por considerar viajar con nosotros.\n Hasta luego.\n");
-      system("exit");
-    }
-
-  }else{
-    if(F<5 && F>=0 && C<3 && C>=0){
-        puts("Su asiento ha sido reservado\n")
-        matriz[F][C]= 'O';
-
-    }else{ 
-      puts("EL numero de filas o columnas esta mal ingresado");
-      printf("¿Deseas continuar con la reserva? (Y/N)\n");
-      getchar();
-      scanf("%c",&s);
-      if (s==Y || s==y){
-        destinoValleDelBravo();
-      }else{
-        printf("Muchas gracias por considerar viajar con nosotros.\n Hasta luego.\n");
-        system("exit");
-      }
-    }
-  }
-
-  for(int i=0;i<5;i++){
-    for(int j=0;j<3;j++){
-        printf("%c",matriz[i][j]);
-    }
-    printf("\n");
-  }
-  
-  return 0;
-}
-
-
-}
-
-
-int registro(){
-
-  FILE *file = fopen("Registro de Usuarios.txt", "w");
-    if (file == NULL){
-      printf("Error al abrir el archivo. \n");
-    }
-    
-      char nombre[100];
-      char correo[100];
-      char telefono[15];
-  
-  printf("...[Registro del usuario]...\n");
-  printf("Ingrese su nombre completo: ");
-  fgets(nombre, sizeof(nombre), stdin);
-  printf("Ingrese su correo electronico: ");
-  fgets(correo, sizeof(correo), stdin);
-  printf("Ingrese su numero de telefono: ");
-  fgets(telefono, sizeof(telefono), stdin);
-  
-  fprintf(file, "Nombre: %s", nombre);
-  fprintf(file, "Correo: %s", correo);
-  fprintf(file, "Telefono: %s", telefono);
-  fclose(file);
-  printf("Su registro se ha concretado\n");
-  
-  
-  return 0;
-}
-
-
 
 
 int destinoCircuitoHistorico(){
- char matriz[10][6]={
-        {'L', 'L', 'L', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', 'L', 'L', 'L'}
-      };
- for(i=0; i<10; i++){
-  for(j=0; j<6; j++){
-    printf("%c", matriz[i][j]);
-  }
 
- }
+  FILE *Historico=fopen("Historico.txt","w");
+  if(Historico==NULL){
+    puts("Error al abrir el archivo");
+    return 1;
+  } 
+  char matriz[10][6]={
+          {'L', 'L', 'L', 'L', 'L', 'L'}, 
+          {'L', 'L', 'L', 'L', 'L', 'L'}, 
+          {'L', 'L', 'L', 'L', 'L', 'L'}, 
+          {'L', 'L', 'L', 'L', 'L', 'L'}, 
+          {'L', 'L', 'L', 'L', 'L', 'L'}, 
+          {'L', 'L', 'L', 'L', 'L', 'L'}, 
+          {'L', 'L', 'L', 'L', 'L', 'L'}, 
+          {'L', 'L', 'L', 'L', 'L', 'L'}, 
+          {'L', 'L', 'L', 'L', 'L', 'L'}, 
+          {'L', 'L', 'L', 'L', 'L', 'L'}
+        };
+  for(int i=0; i<10; i++){
+    for(int j=0; j<6; j++){
+      fprintf(Historico,"%c", matriz[i][j]);
+    }
+  fprintf(Historico,"\n");
+  } 
+  fclose(Historico);
+
+  return 0;
 }
-int destinoTourGastronmico (){
+
+int destinoTourGastronomico (){
+  
+  FILE *Gastronomico=fopen("Gastronomico","w");
+  if(Gastronomico==NULL){
+    puts("Error al abrir el archivo");
+    return 1;
+  }
  char matriz[10][6]={
         {'L', 'L', 'L', 'L', 'L', 'L'}, 
         {'L', 'L', 'L', 'L', 'L', 'L'}, 
@@ -192,9 +88,52 @@ int destinoTourGastronmico (){
         {'L', 'L', 'L', 'L', 'L', 'L'}, 
         {'L', 'L', 'L', 'L', 'L', 'L'}
       };
- 
+  for(int i=0; i<10; i++){
+    for(int j=0; j<6; j++){
+      fprintf(Gastronomico,"%c", matriz[i][j]);
+    }
+  fprintf(Gastronomico,"\n");
+  } 
+  fclose(Gastronomico);
+
+  return 0;
 }
+
 int destinoParqueLaMexicana(){
+  FILE *Parque=fopen("Parque.txt","w");
+  if(Parque==NULL){
+    puts("Error al abrir el archivo");
+    return 1;
+  }
+  char matriz[10][6]={
+        {'L', 'L', 'L', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', 'L', 'L', 'L'}, 
+        {'L', 'L', 'L', 'L', 'L', 'L'}
+      };
+ for(int i=0; i<10; i++){
+    for(int j=0; j<6; j++){
+      fprintf(Parque,"%c", matriz[i][j]);
+    }
+  fprintf(Parque,"\n");
+  } 
+  fclose(Parque);
+
+  return 0;
+}
+
+int destinoUniversum(){
+FILE *Universum=fopen("Universum","w");
+  if(Universum==NULL){
+    puts("Error al abrir el archivo");
+    return 1;
+  }
  char matriz[10][6]={
         {'L', 'L', 'L', 'L', 'L', 'L'}, 
         {'L', 'L', 'L', 'L', 'L', 'L'}, 
@@ -207,25 +146,22 @@ int destinoParqueLaMexicana(){
         {'L', 'L', 'L', 'L', 'L', 'L'}, 
         {'L', 'L', 'L', 'L', 'L', 'L'}
       };
- 
-}
-int destinoUniversum(){
- char matriz[10][7]={
-        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}, 
-        {'L', 'L', 'L', ' ', 'L', 'L', 'L'}
-      };
- 
+  for(int i=0; i<10; i++){
+    for(int j=0; j<6; j++){
+      fprintf(Universum,"%c", matriz[i][j]);
+    }
+  fprintf(Universum,"\n");
+  } 
+  fclose(Universum);
+
+  return 0;
 }
 
 int destinoPuebla(){
+  FILE *Puebla=fopen("Puebla.txt","w");
+if(Puebla==NULL){
+  puts("Error al abrir el archivo");
+}
   char matriz [10][4]={
     {'L', 'L','L', 'L'},
     {'L', 'L','L', 'L'},
@@ -239,9 +175,22 @@ int destinoPuebla(){
     {'L', 'L','L', 'L'}
     
   };
+  for(int i=0; i<10; i++){
+    for(int j=0; j<4; j++){
+      fprintf(Puebla,"%c", matriz[i][j]);
+    }
+  fprintf(Puebla,"\n");
+  } 
+  fclose(Puebla);
+
+  return 0;
 }
 
 int destinoQueretaro(){
+  FILE *Queretaro=fopen("Queretaro.txt","w");
+if(Queretaro==NULL){
+  puts("Error al abrir el archivo");
+}
   char matriz [10][4]={
     {'L', 'L','L', 'L'},
     {'L', 'L','L', 'L'},
@@ -255,9 +204,23 @@ int destinoQueretaro(){
     {'L', 'L','L', 'L'}
     
   };
+  for(int i=0; i<10; i++){
+    for(int j=0; j<4; j++){
+      fprintf(Queretaro,"%c", matriz[i][j]);
+    }
+  fprintf(Queretaro,"\n");
+  } 
+  fclose(Queretaro);
+
+  return 0;
 }
 
 int destinoHidalgo(){
+  FILE *Hidalgo=fopen("Hidalgo.txt","w");
+  if(Hidalgo==NULL){
+    puts("Error al abrir el archivo");
+    return 1;
+  }
   char matriz [10][4]={
     {'L', 'L','L', 'L'},
     {'L', 'L','L', 'L'},
@@ -271,13 +234,22 @@ int destinoHidalgo(){
     {'L', 'L','L', 'L'}
     
   };
+  for(int i=0; i<10; i++){
+    for(int j=0; j<4; j++){
+      fprintf(Hidalgo,"%c", matriz[i][j]);
+    }
+  fprintf(Hidalgo,"\n");
+  } 
+  fclose(Hidalgo);
+  return 0;
 }
 
 int destinoTepotzotlan(){
-    int F, C;
-    char s;
-    char Y='Y';
-    char y='y';
+  FILE *Tepotzotlan=fopen("Tepotzotlan.txt","w");
+  if(Tepotzotlan==NULL){
+    puts("Error al abrir el archivo");
+  return 1;
+  }
    char matriz[5][3] = {
     {'L','L','L'},
     {'L','L','L'},
@@ -287,10 +259,22 @@ int destinoTepotzotlan(){
 
     };
     
-
+    for(int i=0; i<10; i++){
+    for(int j=0; j<6; j++){
+      fprintf(Tepotzotlan,"%c", matriz[i][j]);
+    }
+  fprintf(Tepotzotlan,"\n");
+  } 
+  fclose(Tepotzotlan);
+  return 0;
 }
 
 int destinoValleDelBravo(){
+  FILE *Valle=fopen("Valle.txt","w");
+if(Valle==NULL){
+  puts("Error al abrir el archivo");
+  return 1;
+}
    char matriz[4][2] = {
     {'L','L','L'},
     {'L','L','L'},
@@ -299,92 +283,123 @@ int destinoValleDelBravo(){
     {'L','L','L'}
 
     };
-
     
-   
+    for(int i=0; i<10; i++){
+    for(int j=0; j<6; j++){
+      fprintf(Valle,"%c", matriz[i][j]);
+    }
+  fprintf(Valle,"\n");
+  } 
+  fclose(Valle);
   return 0;
 }
-
-
-int main(){
-  char Tipo;
-
-  puts("Bienvenido a la compañia TravelBus");
-  puts("Explora nuestras opciones de viajes y reserva en minutos\n");
-  puts("¿Desea hacer una reservacion?");
-  puts("Presiona Enter para continuar");
-  getchar();
-
-  registro();
   
-  puts("Estos son nuestros viajes disponibles:\n");
-  puts("a) Viajes turisticos:");
-  puts("b) Viajes nacionales:");
-  puts("c) Pueblos mágicos:");
-  puts("Selecciona una letra");
-  scanf("%c",&Tipo);
   
 
 
-  switch (Tipo){
-    case a:
-      puts("¿A que destino deseas ir?");
-      puts("1.-Circuito Historico");
-      puts("2.-Tour Gastronómico");
-      puts("3.-Parque "La Mexicana"");
-      puts("4.-Universum\n");
-      scanf("%i",&op);
-      switch (op)
-      {
-      case 1:
-        puts("Usted eligió Circuito Historico");
-        
+
+
+int main() {
+    char Tipo;
+    char destino[100];
+    char nombre[100], correo[100], telefono[15];
+    int op, ticket_numero = 1;
+    int continuar = 1;
+
+
+    mkdir("Tickets", 0777);
+
+    puts("Bienvenido a la compañia TravelBus");
+    puts("Explora nuestras opciones de viajes y reserva en minutos\n");
+
+    while (continuar) {
+ 
+        printf("...[Registro del usuario]...\n");
+        printf("Ingrese su nombre completo: ");
+        getchar(); 
+        fgets(nombre, sizeof(nombre), stdin);
+        printf("Ingrese su correo electronico: ");
+        fgets(correo, sizeof(correo), stdin);
+        printf("Ingrese su numero de telefono: ");
+        fgets(telefono, sizeof(telefono), stdin);
+
+     
+        puts("Estos son nuestros viajes disponibles:\n");
+        puts("a) Viajes turisticos:");
+        puts("b) Viajes nacionales:");
+        puts("c) Pueblos mágicos:");
+        puts("Selecciona una letra");
+        scanf(" %c", &Tipo);
+
+        switch (Tipo) {
+             case 'a':
+        puts("¿A que destino deseas ir?");
+        puts("1.-Circuito Historico");
+        puts("2.-Tour Gastronómico");
+        puts("3.-Parque 'La MexicanA'");
+        puts("4.-Universum\n");
+        scanf("%i",&op);
+        if (op == 1){ 
+          strcpy(destino, "Circuito Historico");
+        }else
+        if (op == 2){ 
+          strcpy(destino, "Tour Gastronomico");
+        }else
+        if (op == 3){ 
+          strcpy(destino, "Parque 'La MexicanA'");
+        }else
+        if (op == 4){ 
+          strcpy(destino, "Universum");
+        }
         break;
-      
-      default:
-        break;
-      }
-      break;
 
-    case b:
-      puts("¿A que destino deseas ir?");
-      puts("1.-CDMX-Puebla");
-      puts("2.-CDMX-Queretaro");
-      puts("3.-CDMX-Hidalgo\n");
-      break;
-      
-    case c:
-      puts("¿A que destino deseas ir?");
-      puts("1.-CDMX-Tepoztzotlán");
-      puts("2.-CDMX-Valle de Bravo");
-      break;
-
-    default:
-        puts("Ingresa alguna de esas opciones");
-      break;
-  }
-
-
-  switch (Destino)
-  {
-    case CDMX:
-        int destinoCDMX;
-        if (){
+        puts("¿A que destino deseas ir?");
+        puts("1.-CDMX-Puebla");
+        puts("2.-CDMX-Queretaro");
+        puts("3.-CDMX-Hidalgo\n");
+        scanf("%i",&op);
+        if (op == 1){ 
+          strcpy(destino, "CDMX-Puebla");
+        }else
+        if (op == 2){ 
+          strcpy(destino, "CDMX-Queretaro");
+        }else
+        if (op == 3){ 
+          strcpy(destino, "CDMX-Hidalgo");    
         
         }
-    break;
-
-    case Nacionales:
-          
-    break;
+        break;
         
-    case Pueblos Mágicos:
+
+        case 'c':
+        puts("¿A que destino deseas ir?");
+        puts("1.-CDMX-Tepoztzotlán");
+        puts("2.-CDMX-Valle de Bravo");
+        scanf("%i",&op);
+          if (op == 1){ 
+          strcpy(destino, "CDMX-Tepoztzotlan");
+        }else
+        if (op == 2){ 
+          strcpy(destino, "CDMX-Valle de Bravo");  
           
-    break;
-   
-     default:
-      printf("EL autobus que busca no es del tipo que la empresa tiene");
+        }
+        break;
+
+      default:
+          puts("Ingresa alguna de esas opciones a, b, c");
+        
+        break;
   }
 
-  return 0;
+        if (strlen(destino) > 0) {
+            generar_ticket(nombre, correo, telefono, destino, ticket_numero);
+            ticket_numero++;
+        } 
+
+        printf("¿Desea hacer otra reservación? (1: Sí, 0: No): ");
+        scanf("%d", &continuar);
+    }
+
+    printf("Gracias por usar TravelBus. ¡Buen viaje!\n");
+    return 0;
 }
